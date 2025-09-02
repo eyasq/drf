beeb61657d2498c8b39d412385c26834e00c5748
eyasq <qeremeyas99@gmail.com>
2025-09-02 11:48:34 +0300

    snippet pagination

--- snippets/urls.py
+++ snippets/urls.py
@@ -19,8 +19,9 @@ from django.urls import include, path
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:id>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view())
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root)
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from rest_framework.reverse import reverse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -15,22 +16,32 @@ from rest_framework import permissions
 from snippets.permissions import isOwnerOrReadOnly
 # Create your views here.
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        "user": reverse('user-list', request=request, format=format),
+        "snippet":reverse('snippet-list', request=request, format=format)
+    })
 
-class SnippetList(APIView):
+
+
+class SnippetList(generics.ListCreateAPIView):
     permission_classes=[permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadOnly]
-    def get(self,request):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    def post(self,request):
-        serializer=SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            self.perform_create(serializer=serializer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset=Snippet.objects.all()
+    serializer_class=SnippetSerializer
+    # def get(self,request):
+    #     snippets = Snippet.objects.all()
+    #     serializer = SnippetSerializer(snippets, many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    # def post(self,request):
+    #     serializer=SnippetSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         self.perform_create(serializer=serializer)
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def perform_create(self, serializer):
-        serializer.save(owner = self.request.user)
+    # def perform_create(self, serializer):
+    #     serializer.save(owner = self.request.user)
 
 class SnippetDetail(APIView):
     permission_classes=[permissions.IsAuthenticatedOrReadOnly]
